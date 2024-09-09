b4c0b7c71e71d7f884a3334ee4d9b32153e4d3f1
gurungDev <ngrgurung890@gmail.com>
2024-09-09 17:06:54 +0545

    web seo optimize

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,21 +5,26 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "swiper/css/effect-creative";
-import 'swiper/css/free-mode';
-import 'swiper/css/thumbs';
-import '@/components/ClickableSilder/ClickableSlider.css';
+import "swiper/css/free-mode";
+import "swiper/css/thumbs";
+import "@/components/ClickableSilder/ClickableSlider.css";
 import NavbarComponent from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import "../utils/i18n"; 
+import "../utils/i18n";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Waa Tech Ltd.",
-  description: "Waa Tech Ltd.",
+  description: "Waa Tech Ltd. - Leading provider of web technologies, SEO, and digital marketing services.",
+  keywords:
+    "Waa Tech, web technologies, web development, SEO, digital marketing, website development, web design",
+  openGraph: {
+    locale: "en_US",
+    siteName: "Waa Tech Ltd.", // Corrected from siteName to site_name
+    description: "Waa Tech Ltd. - Web Technologies Services Provider",
+  },
 };
-
-
 
 export default function RootLayout({
   children,
@@ -28,10 +33,42 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content={metadata.description || undefined} />
+        <meta
+          name="keywords"
+          content={
+            typeof metadata.keywords === "string"
+              ? metadata.keywords
+              : metadata.keywords?.join(", ") || undefined
+          }
+        />
+        {metadata.openGraph && (
+          <>
+            <meta
+              property="og:locale"
+              content={metadata.openGraph.locale || undefined}
+            />
+            <meta property="og:url" content={"https://www.waa-tech-ltd.com"} />
+            <meta
+              property="og:site_name"
+              content={metadata.openGraph.siteName || undefined}
+            />
+            <meta property="og:title" content={"Waa Tech Ltd."} />
+            <meta
+              property="og:description"
+              content={metadata.openGraph.description || undefined}
+            />
+          </>
+        )}
+        <title>Waa Tech Ltd.</title>
+      </head>
       <body className={inter.className}>
         <NavbarComponent />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
